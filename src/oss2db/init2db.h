e5bbfdad6a5eb17904b9340fb81ebbb300d3edca
sago007 <sago007@a36be8df-c75f-1e49-46ad-4f0ee64bdc62>
2010-05-19 22:04:49 +0000

    The license notice
    git-svn-id: http://oastat.googlecode.com/svn/trunk@3 a36be8df-c75f-1e49-46ad-4f0ee64bdc62

--- src/oss2db/init2db.h
+++ src/oss2db/init2db.h
@@ -1,9 +1,23 @@
-/* 
- * File:   init2db.h
- * Author: poul
- *
- * Created on 16. april 2010, 22:19
- */
+/*
+===========================================================================
+oastat - OpenArena stat program
+Copyright (C) 2010 Poul Sander (oastat@poulsander.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+===========================================================================
+*/
 
 #ifndef _INIT2DB_H
 #define	_INIT2DB_H
