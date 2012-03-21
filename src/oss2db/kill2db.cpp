3a8d10070d599df8354396b9f174bda346f59e97
sago007 <sago007@a36be8df-c75f-1e49-46ad-4f0ee64bdc62>
2012-03-21 18:45:18 +0000

    Licens notice updated for all files
    git-svn-id: http://oastat.googlecode.com/svn/trunk@45 a36be8df-c75f-1e49-46ad-4f0ee64bdc62

--- src/oss2db/kill2db.cpp
+++ src/oss2db/kill2db.cpp
@@ -1,21 +1,23 @@
 /*
 ===========================================================================
-oastat - OpenArena stat program
-Copyright (C) 2010 Poul Sander (oastat@poulsander.com)
+oastat - a program for parsing log files and write the result to a database
+Copyright (C) 2012 Poul Sander
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
+
+Source information and contacts persons can be found at
+http://code.google.com/p/oastat/
 ===========================================================================
 */
 #include <vector>
