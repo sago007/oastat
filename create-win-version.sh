1ff1f7386b9fb5b48a4d22ba45a14b17923a5c92
sago007 <poul@poulsander.com>
2014-07-02 20:31:23 +0200

    Now copies some files from mxe that are needed for the distribution

--- create-win-version.sh
+++ create-win-version.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -e
 cd sql/create_tables/
 ./create_specific.sh
 cd ../../src
@@ -7,3 +9,7 @@ mkdir -p oastat-win
 cd oastat-win
 cp ../sql/create_tables/oastat_create_tables_sqlite.sql ./
 cp ../src/oastat.exe ./
+cp ~/git/mxe/usr/i686-pc-mingw32/bin/libcppdb.dll ./
+cp ~/git/mxe/usr/i686-pc-mingw32/bin/libcppdb_sqlite3.dll ./
+cp ~/git/mxe/usr/i686-pc-mingw32/bin/sqlite3.exe ./
+cp ../COPYING ./COPYING.txt
