554307deec92f16e1ba026fdc1b191b80a299959
sago007 <poul@poulsander.com>
2018-11-10 16:02:53 +0100

    Updated URL in all files

--- src/oss2db/kill2db.cpp
+++ src/oss2db/kill2db.cpp
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see http://www.gnu.org/licenses/
 
 Source information and contacts persons can be found at
-http://code.google.com/p/oastat/
+https://github.com/sago007/oastat/
 ===========================================================================
 */
 #include <vector>
